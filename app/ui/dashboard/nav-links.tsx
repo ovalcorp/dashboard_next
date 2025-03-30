d2a65bb64fec116b50d76b54fff0c11c18b7953c
ovalcorp <luis.pozo.f@uni.pe>
2025-03-30 18:42:47 -0500

    implementar bcryptjs v5

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -4,7 +4,7 @@ import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
-} from '@heroicons/react/24/outline';
+} from '@heroicons/react/24/solid';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
